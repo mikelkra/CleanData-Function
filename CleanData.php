6d07b867b20ce1c97d0cb34fed1b3c1d8adb2625
mikelkra <mikelkrasniqi@gmail.com>
2018-01-25 20:32:36 +0100

    Clean up

--- CleanData.php
+++ CleanData.php
@@ -1,28 +1,22 @@
-<?php
-		function cleanThisData($data) {
-			if (is_array ( $data )) {
-				// if data is array then do the mapping and send the values back to the same function one by one
-				$cleanData = array_map ( 'cleanThisData', $data );
-			} else {
-				
-				if (filter_var ( $data, FILTER_VALIDATE_EMAIL )) {
-					// if EMAIL
-					$cleanData = filter_var ( $data, FILTER_SANITIZE_EMAIL );
-				}			
-				
-				elseif (filter_var ( $data, FILTER_VALIDATE_URL )) {
-					// if URL
-					$cleanData = filter_var ( $data, FILTER_SANITIZE_URL );
-				}			
-				
-				else {
-					// if anything else (string, ip ...)
-					$cleanData = filter_var ( $data, FILTER_SANITIZE_STRIPPED );
-					$cleanData = filter_var ( $cleanData, FILTER_SANITIZE_STRING );
-					$cleanData = filter_var ( $cleanData, FILTER_SANITIZE_SPECIAL_CHARS );
-				}
-			}
-			return $cleanData;
-			
+function cleanData($data) {
+	if (is_array ( $data )) {
+		// if array 
+		$cleanData = array_map ( 'cleanData', $data );
+	} else {
+		
+		if (filter_var ( $data, FILTER_VALIDATE_EMAIL )) {
+			// if EMAIL
+			$cleanData = filter_var ( $data, FILTER_SANITIZE_EMAIL );
+		} elseif (filter_var ( $data, FILTER_VALIDATE_URL )) {
+			// if URL
+			$cleanData = filter_var ( $data, FILTER_SANITIZE_URL );
+		} else {
+			// if anything else (string, ip ...)
+			$cleanData = filter_var ( $data, FILTER_SANITIZE_STRIPPED );
+			$cleanData = filter_var ( $cleanData, FILTER_SANITIZE_STRING );
+			$cleanData = filter_var ( $cleanData, FILTER_SANITIZE_SPECIAL_CHARS );
 		}
-?>
+	}
+	return $cleanData;
+}
+
